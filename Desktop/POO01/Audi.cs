2c4c9cd66bb0a9fddb32208f46ad9854b0337899
Elnatan Torres | Stone <etorres@stone.com.br>
2020-08-30 22:47:05 -0300

    Fix encapsulation

--- Desktop/POO01/Audi.cs
+++ Desktop/POO01/Audi.cs
@@ -4,20 +4,16 @@ namespace teste04 {
    
     public class Audi : Car
     {
-        private int Velocity  { get; set; } // Velocidade
         public void MotorAudi()
         {
             Console.WriteLine("Audi exclusive engine.");
         }
+
         public void Accelerate()
         {
-            Velocity=10;
+            Velocity = 10;
+
             Console.WriteLine($"The car is Accelerating {Velocity}.");
         }
-
-        
-
     }
-    
 }
-
--- Desktop/POO01/Car.cs
+++ Desktop/POO01/Car.cs
@@ -12,7 +12,7 @@ namespace teste04
        public int Height { get; set; } // Altura
        public string Color { get; set; } // Cor
        public int RimSize { get; set; } // Rodas
-
+       protected int Velocity  { get; set; } // Velocidade
 
        #endregion
        
@@ -20,31 +20,24 @@ namespace teste04
          #region Behaviors
        
        public void RampUp(){
-           
 
-           Console.WriteLine($"The {Name} car of {Color} color with {Length} meters long and {Height} meters high is built with {RimSize} wheels.");
+           Console.WriteLine($"The car {Name} is Built.");
        }
 
        public void TurnOn(){
            
-           Console.WriteLine($"The {Name} car is started");
+           Console.WriteLine($"The car {Name} is started");
        }
 
        public void Sell(){
 
-           Console.WriteLine($"The {Name} car is Sold.");
+           Console.WriteLine($"The car {Name} is Sold.");
 
            } 
        public void Drive(){
 
-           Console.WriteLine($"The {Name} car is Driven.");
+           Console.WriteLine($"The car {Name} is Driven.");
        }
-
-       public void Accelerate(){
-
-           Console.WriteLine($"The {Name} car is accelerating.");
-           
-       } 
        
        #endregion
        
